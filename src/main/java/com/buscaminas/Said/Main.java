85f1717ae0bd324b7c0362fa7131b9238ddb21f0
jaine <jaine@JASA>
2025-07-30 21:07:25 -0600

    Primer commit "Se creo el proyecto y  dos clases"

---
+++ src/main/java/com/buscaminas/Said/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.buscaminas.Said;
+
+/**
+ *
+ * @author jaine
+ */
+public class Main {
+    public class Buscaminas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+}
