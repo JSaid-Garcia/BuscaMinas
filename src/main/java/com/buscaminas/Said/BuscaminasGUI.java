de801fb9309e9856e92d03a196b8f18d11fcb64a
jaine <jaine@JASA>
2025-08-03 15:57:45 -0600

    Septimo commit "Se implemento los importes que vamos a usar en la clase BuscaminasGUI y los componentes de la interfaz grafica"

--- src/main/java/com/buscaminas/Said/BuscaminasGUI.java
+++ src/main/java/com/buscaminas/Said/BuscaminasGUI.java
@@ -4,10 +4,29 @@
  */
 package com.buscaminas.Said;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 /**
- *
- * @author jainer Said
+ * Clase que maneja la interfaz gráfica del juego Buscaminas
+ * Universidad Técnica Nacional ITI
+ * Programación I - Proyecto Final
+ * 
+ * @author Said
+ * @version 1.0
+ * @since 2025-07-27
  */
-public class BuscaminasGUI {
+public class BuscaminasGUI extends JFrame {
+    // Componentes de la interfaz gráfica
+    private BuscaminasModelo modelo;
+    private JButton[][] botones;
+    private JLabel labelContadorMinas;
+    private JLabel labelEstado;
+    private JPanel panelTablero;
     
+  
 }
